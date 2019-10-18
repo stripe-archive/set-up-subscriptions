4ecfa1e534995d756491faf89a1fc5c9f55f963d
Christopher Trudeau <ctrudeau@stripe.com>
2019-10-17 17:46:54 -0700

    Fixing path variables

--- server/python/server.py
+++ server/python/server.py
@@ -13,21 +13,27 @@ import os
 from flask import Flask, render_template, jsonify, request, send_from_directory
 from dotenv import load_dotenv, find_dotenv
 
-static_dir = f'{os.path.abspath(os.path.join(__file__ ,"../../../client"))}'
-app = Flask(__name__, static_folder=static_dir, static_url_path="", template_folder=static_dir)
 
 # Setup Stripe python client library
 load_dotenv(find_dotenv())
 stripe.api_key = os.getenv('STRIPE_SECRET_KEY')
 stripe.api_version = os.getenv('STRIPE_API_VERSION')
 
+static_dir = str(os.path.abspath(os.path.join(
+    __file__, "..", os.getenv("STATIC_DIR"))))
+app = Flask(__name__, static_folder=static_dir,
+            static_url_path="", template_folder=static_dir)
+
+
 @app.route('/', methods=['GET'])
 def get_index():
     return render_template('index.html')
 
+
 @app.route('/public-key', methods=['GET'])
 def get_public_key():
     return jsonify(publicKey=os.getenv('STRIPE_PUBLIC_KEY'))
+
 
 @app.route('/create-customer', methods=['POST'])
 def create_customer():
@@ -38,10 +44,10 @@ def create_customer():
     try:
         # This creates a new Customer and attaches the PaymentMethod in one API call.
         customer = stripe.Customer.create(
-            payment_method=paymentMethod, 
+            payment_method=paymentMethod,
             email=data['email'],
             invoice_settings={
-                'default_payment_method':paymentMethod
+                'default_payment_method': paymentMethod
             }
         )
         # At this point, associate the ID of the Customer object with your
@@ -62,6 +68,7 @@ def create_customer():
     except Exception as e:
         return jsonify(e), 403
 
+
 @app.route('/subscription', methods=['POST'])
 def getSubscription():
     # Reads application/json and returns a response
@@ -71,6 +78,7 @@ def getSubscription():
         return jsonify(subscription)
     except Exception as e:
         return jsonify(e), 403
+
 
 @app.route('/webhook', methods=['POST'])
 def webhook_received():
@@ -95,33 +103,33 @@ def webhook_received():
         event_type = request_data['type']
 
     data_object = data['object']
-    
+
     if event_type == 'customer.created':
-        # print(data)
+        print(data)
 
     if event_type == 'customer.updated':
-        # print(data)
+        print(data)
 
     if event_type == 'invoice.upcoming':
-        # print(data)
+        print(data)
 
     if event_type == 'invoice.created':
-        # print(data)
+        print(data)
 
     if event_type == 'invoice.finalized':
-        # print(data)
+        print(data)
 
     if event_type == 'invoice.payment_succeeded':
-        # print(data)
+        print(data)
 
     if event_type == 'invoice.payment_failed':
-        # print(data)
+        print(data)
 
     if event_type == 'customer.subscription.created':
-        # print(data)
+        print(data)
 
     return jsonify({'status': 'success'})
 
 
-if __name__== '__main__':
+if __name__ == '__main__':
     app.run(port=4242)
