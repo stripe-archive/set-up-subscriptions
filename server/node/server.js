4d1daa4f51e22583570a81e81a8b4c3fa03b7945
GA1 <nvkga1@gmail.com>
2020-06-09 00:25:55 +0200

    Add full link to local host instance for node

--- server/node/server.js
+++ server/node/server.js
@@ -123,4 +123,4 @@ app.post('/webhook', async (req, res) => {
   res.sendStatus(200);
 });
 
-app.listen(4242, () => console.log(`Node server listening on port ${4242}!`));
+app.listen(4242, () => console.log(`Node server listening on port http://localhost:${4242}!`));
